3bff91718c17922b29c733b09fbe0473f58036ca
Siraj Preena <sirajpreena311@gmail.com>
2025-09-22 00:08:43 +0530

    cart

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,9 +1,37 @@
 const asyncHandler = require('express-async-handler')
+const User = require('../models/User')
 
 // POST /cart/addtocart
 const addToCart = asyncHandler(async (req, res) => {
-  
+  const { itemId } = req.body
+
+  if (!itemId) {
+    return res.status(400).json({ message: "Item ID is required" });
+  }
+
+  await User.findOneAndUpdate(
+    { _id: req.user.id },
+    { $inc: { [`cartData.${itemId}`]: 1 } }
+  );
+
+  res.json({ message: 'Item added to cart' })
 })
+
+// POST /cart/removefromcart
+const removeFromCart = asyncHandler(async (req, res) => {
+  const { itemId } = req.body;
+
+  if (!itemId) {
+    return res.status(400).json({ message: "Item ID is required" });
+  }
+
+  await User.findOneAndUpdate(
+    { _id: req.user.id, [`cartData.${itemId}`]: { $gt: 0 } }, // only if qty > 0
+    { $inc: { [`cartData.${itemId}`]: -1 } }
+  )
+
+  res.json({ message: "Item removed from cart" })
+});
 
 module.exports = {
   addToCart
